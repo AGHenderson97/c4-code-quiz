bdb91a2f9650d4462380081bea59c4f5ed4e2c3e
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-17 21:24:03 -0400

    quiz questions

---
+++ script.js
@@ -0,0 +1,31 @@
+// Set up quiz questions
+var questions = [
+    {
+      question: "What does HTML stand for?",
+      answers: [
+        { text: "Hyper Text Markup Language", correct: true },
+        { text: "Hyperlinks and Text Markup Language", correct: false },
+        { text: "Home Tool Markup Language", correct: false },
+        { text: "High Text Markup Language", correct: false }
+      ]
+    },
+    {
+      question: "What does CSS stand for?",
+      answers: [
+        { text: "Creative Style Sheets", correct: false },
+        { text: "Cascading Style Sheets", correct: true },
+        { text: "Computer Style Sheets", correct: false },
+        { text: "Colorful Style Sheets", correct: false }
+      ]
+    },
+    {
+      question: "What does JS stand for?",
+      answers: [
+        { text: "Java Symbol", correct: false },
+        { text: "Java Syntax", correct: false },
+        { text: "JavaScript", correct: true },
+        { text: "Java Statement", correct: false }
+      ]
+    }
+  ];
+  
